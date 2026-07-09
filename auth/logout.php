2d59d9e4bff07461d9cb27892c36f9bb02f7d222
Kaaaaan <kenbautista753@gmail.com>
2026-07-10 01:35:11 +0800

    create: add logout page

---
+++ auth/logout.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/../includes/session.php';
+require_once __DIR__ . '/../database/connection.php';
+
+clear_remember_login($conn);
+clear_login_session();
+
+$_SESSION = [];
+
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
+    unset($_COOKIE[session_name()]);
+}
+
+session_destroy();
+
+header('Location: ../index.php');
+exit;
